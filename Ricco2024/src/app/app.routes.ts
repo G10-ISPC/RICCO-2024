b2c39d1c900a5036bca7e19871080bfe6736a8a8
LAURA PATRICIA CRUZ <95201770+LauraPatriciaCruz@users.noreply.github.com>
2024-05-11 18:10:39 -0300

    CREE COMPONENTE PRODUCTOS, Y ACTUALICÉ PAGINA INICIO EN COMPONENTE POR DEFAULT, TRASLADÉ LA CARPETA IMAGENES Y CSS

--- Ricco2024/src/app/app.routes.ts
+++ Ricco2024/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ProductosComponent } from
+ './pages/productos/productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"productos", component:ProductosComponent},
+];
