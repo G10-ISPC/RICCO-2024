a2014ca8aefda77b8954201334ab2de30b89548d
carla <arevalocarlaelizabethmail.com>
2024-05-11 21:56:33 -0300

    Se migro lo de la pag. principal al componente home, se creo componente header. También se puso en funcionalidad para los demas componentes

--- Ricco2024/src/app/app.routes.ts
+++ Ricco2024/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { ProductosComponent } from
- './pages/productos/productos.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 export const routes: Routes = [
-    {path:"productos", component:ProductosComponent},
+    {path: 'productos', component: ProductosComponent},
+    {path: 'home', component: HomeComponent},
 ];
