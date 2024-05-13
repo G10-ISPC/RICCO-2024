033b7dce2c7688e35067dedefde8386b027f9b07
MelisaSG <kianetomoe@gmail.com>
2024-05-13 04:01:03 -0300

    se visualiza registro correctamente

--- Ricco2024/src/app/app.routes.ts
+++ Ricco2024/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { ProductosComponent } from './pages/productos/productos.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 
 export const routes: Routes = [
+    {path: "", redirectTo: "/home", pathMatch: "full" },
     {path: 'productos', component: ProductosComponent},
     {path: 'home', component: HomeComponent},
     {path: 'nosotros', component: NosotrosComponent},
     {path: 'contacto', component: ContactoComponent},
+    {path: 'auth', component: AuthComponent}
 ];
