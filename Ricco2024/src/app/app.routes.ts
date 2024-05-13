dc7d92a20f0c1397ba79c009f35369c03fdf71b7
Antonio Figueroa <a_figueroa@yahoo.com>
2024-05-13 00:55:23 -0300

    Creacion componente carrito

--- Ricco2024/src/app/app.routes.ts
+++ Ricco2024/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
 
 
 
@@ -9,4 +10,5 @@ export const routes: Routes = [
     {path: 'productos', component: ProductosComponent},
     {path: 'home', component: HomeComponent},
     {path: 'nosotros', component: NosotrosComponent},
+    {path: 'carrito', component: CarritoComponent},
 ];
