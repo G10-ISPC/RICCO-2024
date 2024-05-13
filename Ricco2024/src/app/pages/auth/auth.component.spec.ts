51d053e2371fd3f8da90d781a9d31cf761f19854
Noobuyer <cevascoagustin@gmail.com>
2024-05-12 22:23:01 -0300

    cambios en login

--- Ricco2024/src/app/pages/auth/auth.component.spec.ts
+++ Ricco2024/src/app/pages/auth/auth.component.spec.ts
@@ -22,29 +22,3 @@ describe('AuthComponent', () => {
   });
 });
 
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent {
-  username: string = '';
-  password: string = '';
-  loginError: boolean = false;
-
-  constructor(private router: Router) {}
-
-  login() {
-    // Aquí puedes implementar la lógica de autenticación
-    if (this.username === 'usuario' && this.password === 'contraseña') {
-      // Autenticación exitosa, redirige a otra página
-      this.router.navigate(['/dashboard']);
-    } else {
-      // Muestra un mensaje de error
-      this.loginError = true;
-    }
-  }
-}
