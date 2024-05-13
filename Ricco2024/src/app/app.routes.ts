0c4687cad5ee7aeb7ebf5ecbba9742d1c9aaa2ed
Noobuyer <cevascoagustin@gmail.com>
2024-05-12 22:36:22 -0300

    ruteo de login

--- Ricco2024/src/app/app.routes.ts
+++ Ricco2024/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { LoginComponent } from './pages/auth/auth_login/login/login.component';
 
 
 
@@ -9,4 +10,5 @@ export const routes: Routes = [
     {path: 'productos', component: ProductosComponent},
     {path: 'home', component: HomeComponent},
     {path: 'nosotros', component: NosotrosComponent},
+    {path: 'login', component: LoginComponent},
 ];
