942784fb0731b7a55a9340c0336f720f83febc6b
Delfina Aricoma <delrcmmamani@gmail.com>
2024-05-13 00:30:17 -0300

    Subo pagina inicios con las rutas realizadas

--- Ricco2024/src/app/app.routes.ts
+++ Ricco2024/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
-
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 
 export const routes: Routes = [
     {path: 'productos', component: ProductosComponent},
     {path: 'home', component: HomeComponent},
     {path: 'nosotros', component: NosotrosComponent},
+    {path: 'contacto', component: ContactoComponent},
 ];
