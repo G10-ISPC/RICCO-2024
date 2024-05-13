a751fc2f00348e3a3a6e22b560cbf878c93bf1e6
MelisaSG <kianetomoe@gmail.com>
2024-05-13 03:02:07 -0300

    se agrega visualizacion de la pagina de registro

--- Ricco2024/src/app/app.routes.ts
+++ Ricco2024/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 
 
@@ -9,4 +10,5 @@ export const routes: Routes = [
     {path: 'productos', component: ProductosComponent},
     {path: 'home', component: HomeComponent},
     {path: 'nosotros', component: NosotrosComponent},
+    {path: 'auth', component: AuthComponent},
 ];
