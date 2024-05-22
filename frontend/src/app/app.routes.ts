7bb5666dc290cbe3d4717c302065de0a4fe4f5fe
LAURA PATRICIA CRUZ <95201770+LauraPatriciaCruz@users.noreply.github.com>
2024-05-22 19:34:09 -0300

    AGREGUÉ PAGINA ERROR404 INCLUYENDO RUTAS DE COMPONENTES Y FOTOS

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { CarritoComponent } from './carrito/carrito.component';
-
+import { Error404Component } from './error404/error404.component';
 
 
 import { RegistroComponent } from './registro/registro.component';
@@ -21,6 +21,6 @@ export const routes: Routes = [
     { path: 'contacto', component: ContactoComponent },
     { path: 'carrito', component: CarritoComponent },
     { path: 'registro', component: RegistroComponent },
-    { path: 'dashboard', component: DashboardComponent }
-
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '**', component: Error404Component },
 ];
