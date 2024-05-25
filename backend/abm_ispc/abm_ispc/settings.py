c6197be2bf622b948ced0493bdcfaa771b338aba
unknown <cosmarian@gmail.com>
2024-05-24 22:34:24 -0300

    Backend-Subo creación de varias APIS

--- backend/abm_ispc/abm_ispc/settings.py
+++ backend/abm_ispc/abm_ispc/settings.py
@@ -55,7 +55,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MiprimerABM'
+    'MiprimerABM',
+     'rest_framework',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +68,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost',
+    ]
 
 ROOT_URLCONF = 'abm_ispc.urls'
 
