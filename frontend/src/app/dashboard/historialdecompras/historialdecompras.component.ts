2c8fd84cd5b6be8a3df4b4dfb78bdca9a3d78653
Mica <micajuarez26@hotmail.com>
2024-06-02 18:38:39 -0300

    Dashboard

---
+++ frontend/src/app/dashboard/historialdecompras/historialdecompras.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DashboardComponent } from '../dashboard.component';
+
+@Component({
+  selector: 'app-historialdecompras',
+  standalone: true,
+  imports: [DashboardComponent],
+  templateUrl: './historialdecompras.component.html',
+  styleUrl: './historialdecompras.component.css'
+})
+export class HistorialdecomprasComponent {
+
+}
