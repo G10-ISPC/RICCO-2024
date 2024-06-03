d081c713984ad707d51d0fa6a360996538700246
LAURA PATRICIA CRUZ <95201770+LauraPatriciaCruz@users.noreply.github.com>
2024-06-03 09:21:06 -0300

    subo cambios en componente cart (carrito)

--- frontend/src/app/cart/components/amount-adjuster/amount-adjuster.component.ts
+++ frontend/src/app/cart/components/amount-adjuster/amount-adjuster.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-amount-adjuster',
   standalone: true,
   imports: [],
   templateUrl: './amount-adjuster.component.html',
-  styleUrl: './amount-adjuster.component.css'
+  styleUrl: './amount-adjuster.component.css',
 })
-export class AmountAdjusterComponent {
+export class AmountAdjusterComponent{
+  @Input() quantity: number = 1;
+  @Output() quantityChange = new EventEmitter<number>();
 
+  onIncreaseQuantity() {
+    this.quantityChange.next(this.quantity + 1);
+  }
+
+  onDecreaseQuantity() {
+    if (this.quantity > 1) this.quantityChange.next(this.quantity - 1);
+  }
 }
