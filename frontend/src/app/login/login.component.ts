d0fe02f72bfb0943336ee578f51eca308bbef94d
Dalma <p.d.f.1915@gmail.com>
2024-06-03 12:12:46 -0300

    se inyecta servicio login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,30 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../services/auth/login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  form!: FormGroup;
+  signIn = inject(LoginService);
+  router = inject(Router);
+
+  loginForm: FormGroup;
+
   constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group(
+    this.loginForm = this.formBuilder.group(
       {
-        
+
         email: ['', [Validators.required, Validators.email], []],
 
         password: ['', [Validators.required], []],
-        
+
       }
     )
   }
 
-  onEnviar(event: Event) {
-    console.log(this.form.value)
+  get Email() {
+
+    return this.loginForm.get("email");
+
+  }
+
+  get Password() {
+
+    return this.loginForm.get("password");
+
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+
+      this.signIn.login(this.loginForm.value);
+      this.router.navigate(['/']);
+      this.loginForm.reset();
+
+    } else {
+
+      this.loginForm.markAllAsTouched();
+
+      alert("Error al ingresar los datos")
+    }
   }
 
 
