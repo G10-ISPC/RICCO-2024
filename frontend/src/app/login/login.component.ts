98c9a1d3a905953929e7c312707cda34c651515a
carla <arevalocarlaelizabeth@gmail.com>
2024-06-03 21:09:26 -0300

    modificaciones en login. Fuciona el get, falta ver post

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,30 +1,72 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,  OnInit} from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { LogService } from '../services/log.service';
+import { LogRequest } from '../data/logRequest';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
-  form!: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group(
-      {
-        
-        email: ['', [Validators.required, Validators.email], []],
+  loginError: string = "";
+  loginForm: FormGroup;
+  
+  constructor(private formBuilder:FormBuilder, private router:Router, private logService: LogService){
 
-        password: ['', [Validators.required], []],
-        
-      }
-    )
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
   }
 
-  onEnviar(event: Event) {
-    console.log(this.form.value)
+  ngOnInit(): void {}
+
+  get email()
+  {
+    return this.loginForm.get('email');
+  }
+
+  get password()
+  {
+    return this.loginForm.get('password');
+  }
+
+  login(): void {
+    if (this.loginForm.valid) {
+      const credentials: LogRequest = this.loginForm.value;
+      this.logService.login(credentials).subscribe({
+    // if(this.loginForm.valid){
+    //   this.logService.login(this.loginForm.value as LogRequest).subscribe({
+        next: (userData) =>{
+          console.log("usuario autenticado.",userData);
+          this.router.navigateByUrl('/home');
+          this.loginForm.reset(); 
+        },
+        error: (error) =>{
+          console.error("Error en el inicio de sesión.",error);
+          this.loginError=error.message;
+        },
+        complete: () => {
+          console.info("Login completo");
+ 
+        }
+        
+      })
+
+      }
+
+    
+    else{
+      this.loginForm.markAllAsTouched();
+      alert("Por favor, complete todos los campos.");
+    }
   }
 
 
