88019555bf12b01d1be588076e2a7c1152fc0fb2
unknown <cosmarian@gmail.com>
2024-06-09 10:59:11 -0300

    Subo modificaciones en frontend:log.service, creacion de interceptors y proxy

--- frontend/src/app/nav/nav.component.ts
+++ frontend/src/app/nav/nav.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { LogService } from '../services/log.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
+constructor (private logService: LogService, 
+  private route: Router
+){}
+readonly isUserLogin$=this.logService.isUserLogin();
+logout():void{
+  this.logService.logout();
+  this.route.navigateByUrl("/login")
+}
 }
