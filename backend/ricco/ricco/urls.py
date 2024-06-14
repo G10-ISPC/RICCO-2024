d7c3844cb282cc72eaa7db320db6f01024726c34
carla <arevalocarlaelizabeth@gmail.com>
2024-06-14 04:06:08 -0300

    elimino codigo comentado

--- backend/ricco/ricco/urls.py
+++ backend/ricco/ricco/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework import routers
 
-# Api router
-# router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
